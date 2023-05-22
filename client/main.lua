b71fb857a39e71fdcec70d833a6c34e6ba03c883
Qubit <13704525+qubitrepos@users.noreply.github.com>
2023-05-22 14:12:55 +0300

    Several refactor maded

--- client/main.lua
+++ client/main.lua
@@ -6,17 +6,15 @@ Citizen.CreateThread(function()
     local sleep = 1000
 	while true do
         local playerPed = PlayerPedId()
-        local vehicle = GetVehiclePedIsIn(playerPed, false)
-        if DoesEntityExist(vehicle) then
+        if IsPedInAnyVehicle(playerPed) then
             sleep = 100
-            local shakeRate = GetEntitySpeed(vehicle) / 250.0
-            if vehicle ~= nil and vehicle ~= 0 then
-                local curHealth = GetVehicleBodyHealth(vehicle)
-                if curHealth ~= lastDamage then
-                    ShakeGameplayCam("MEDIUM_EXPLOSION_SHAKE", shakeRate)
-                end
-                lastDamage = curHealth
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+            local shakeRate = GetEntitySpeed(vehicle) / 250.0 -- 250.0 Normal Shake / 100.0 High Shake / 50.0 Maksimum Shake
+            local curHealth = GetVehicleBodyHealth(vehicle)
+            if curHealth ~= lastDamage then
+                ShakeGameplayCam("MEDIUM_EXPLOSION_SHAKE", shakeRate)
             end
+            lastDamage = curHealth
         else
             sleep = 1000
         end
