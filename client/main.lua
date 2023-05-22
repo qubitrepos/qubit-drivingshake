93132e73a3ef67f75cce30130b8f035950b10437
Qubit <13704525+qubitrepos@users.noreply.github.com>
2023-05-22 04:01:48 +0300

    Merging

--- client/main.lua
+++ client/main.lua
@@ -5,20 +5,21 @@ local vehicle = nil
 Citizen.CreateThread(function()
   local sleep = 1000
 	while true do
-        local veh2 = GetVehiclePedIsIn(PlayerPedId(), false)
-        vehicle = GetVehiclePedIsIn(PlayerPedId(-1), false)
-        if DoesEntityExist(vehicle) and vehicle then
+        local playerPed = PlayerPedId()
+        local vehicle = GetVehiclePedIsIn(playerPed, false)
+        if DoesEntityExist(vehicle) then
             sleep = 100
-            local veh = GetVehiclePedIsIn(GetPlayerPed(-1))
-			      local shakeRate = GetEntitySpeed(veh) / 250.0
-            lastdamage = curdamage
-            curdamage = GetVehicleBodyHealth(vehicle)
-            if curdamage ~= lastdamage then
-                ShakeGameplayCam("MEDIUM_EXPLOSION_SHAKE", shakeRate)
+            local shakeRate = GetEntitySpeed(vehicle) / 250.0
+            if vehicle ~= nil and vehicle ~= 0 then
+                local curHealth = GetVehicleBodyHealth(vehicle)
+                if curHealth ~= lastDamage then
+                    ShakeGameplayCam("MEDIUM_EXPLOSION_SHAKE", shakeRate)
+                end
+                lastDamage = curHealth
             end
         else
             sleep = 1000
         end
-  Citizen.Wait(sleep)
+        Citizen.Wait(sleep)
 	end
 end)
