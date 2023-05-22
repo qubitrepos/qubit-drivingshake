8eef42b8d8c84fcbb734e62f996e916f27f125a5
bitc0de <keyvenm@gmail.com>
2023-05-22 02:51:35 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -4,21 +4,23 @@ local curspeed = 0.0
 local vehicle
 
 Citizen.CreateThread(function()
+    local sleep = 1000
 	while true do
         local veh2 = GetVehiclePedIsIn(PlayerPedId(), false)
         vehicle = GetVehiclePedIsIn(PlayerPedId(-1), false)
         if DoesEntityExist(vehicle) and vehicle then
+            sleep = 100
             local veh = GetVehiclePedIsIn(GetPlayerPed(-1))
 			local shakeRate = GetEntitySpeed(veh) / 250.0
-            print(shakeRate)
             lastdamage = curdamage
             curdamage = GetVehicleBodyHealth(vehicle)
             if curdamage ~= lastdamage then
                 ShakeGameplayCam("MEDIUM_EXPLOSION_SHAKE", shakeRate)
             end
         else
-            Citizen.Wait(1000)
+            sleep = 1000
         end
-        Citizen.Wait(0)
+        Citizen.Wait(sleep)
 	end
 end)
+
