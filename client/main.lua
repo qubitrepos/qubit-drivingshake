4b48fd99880c1367f79f87859250504cba791f22
Qubit <13704525+qubitrepos@users.noreply.github.com>
2023-05-22 14:12:34 +0300

    Revert "Several refactor maded"
    This reverts commit daeeea0c952ea63324566491248bf26d2a4f2d64.

--- client/main.lua
+++ client/main.lua
@@ -6,15 +6,17 @@ Citizen.CreateThread(function()
     local sleep = 1000
 	while true do
         local playerPed = PlayerPedId()
-        if IsPedInAnyVehicle(playerPed) then
+        local vehicle = GetVehiclePedIsIn(playerPed, false)
+        if DoesEntityExist(vehicle) then
             sleep = 100
-            local vehicle = GetVehiclePedIsIn(playerPed, false)
-            local shakeRate = GetEntitySpeed(vehicle) / 250.0 -- 250.0 Normal Shake / 100.0 High Shake / 50.0 Maksimum Shake
-            local curHealth = GetVehicleBodyHealth(vehicle)
-            if curHealth ~= lastDamage then
-                ShakeGameplayCam("MEDIUM_EXPLOSION_SHAKE", shakeRate)
+            local shakeRate = GetEntitySpeed(vehicle) / 250.0
+            if vehicle ~= nil and vehicle ~= 0 then
+                local curHealth = GetVehicleBodyHealth(vehicle)
+                if curHealth ~= lastDamage then
+                    ShakeGameplayCam("MEDIUM_EXPLOSION_SHAKE", shakeRate)
+                end
+                lastDamage = curHealth
             end
-            lastDamage = curHealth
         else
             sleep = 1000
         end
